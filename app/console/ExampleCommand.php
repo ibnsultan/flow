076a2ffda6e42d8469a0f89a638db4c4f4ce7810
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-12 20:46:03 +0300

    ✨ Console application files

---
+++ app/console/ExampleCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console;
+
+use Aloe\Command;
+
+class ExampleCommand extends Command
+{
+    protected static $defaultName = 'example';
+    public $description = 'example command\'s description';
+    public $help = 'example command\'s help';
+
+    protected function config()
+    {
+        $this
+            ->setArgument('argument', 'optional', 'argument description')
+            ->setOption('option', 'o', 'required', 'option description');
+    }
+
+    protected function handle()
+    {
+        $this->comment(
+            "example command's output {$this->argument('argument')} {$this->option('option')}"
+        );
+
+        return 0;
+    }
+}
