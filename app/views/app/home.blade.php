1df02305f16f5ed1566c388ff8720d2f390eaed0
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-12 21:09:55 +0300

    ✨ User app home view

---
+++ app/views/app/home.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.app')	
+@section('content')
+	<div class="pc-container">
+		<div class="pc-content">
+			<div class="container-fluid">
+
+                <div class="row">
+                
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header m-0">
+                                <h3 class="card-title">Welcome to <span class="text-primary">Flow</span></h3>
+                            </div>
+                            <div class="card-body">
+                                <p>Flow is a comprehensive web application development starter kit designed to accelerate the process of building modern and feature-rich web applications. With a focus on simplicity, flexibility, and efficiency, Flow provides developers with a robust set of tools and features to kickstart their projects. From secure authentication and seamless API integration to beautifully designed UI/UX components and powerful admin tools</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row mt-2">
+                
+                    <div class="col-xl-3 col-md-6 col-sm-12 mt-3">
+                        <div class="card h-100">
+                            <div class="card-body">
+                            
+                                <h5 class="card-title mb-4">
+                                    <i class="bi bi-shield-lock"></i> &nbsp; Authentication
+                                </h5>
+
+                                <p>Flow provides robust authentication mechanisms, ensuring that only authorized users can access your application's resources</p>
+
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="col-xl-3 col-md-6 col-sm-12 mt-3">
+                        <div class="card h-100">
+                            <div class="card-body">
+                            
+                                <h5 class="card-title mb-4">
+                                    <i class="bi bi-link-45deg"></i> &nbsp; API Integration
+                                </h5>
+
+                                <p>Flow comes equipped with a versatile and ready-to-use API, allowing seamless integration with external systems and services</p>
+
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="col-xl-3 col-md-6 col-sm-12 mt-3">
+                        <div class="card h-100">
+                            <div class="card-body">
+                            
+                                <h5 class="card-title mb-4">
+                                    <i class="bi bi-palette"></i> &nbsp; Well-Designed UI/UX
+                                </h5>
+
+                                <p> With Flow, you get access to a collection of well-designed UI components and intuitive UX patterns. From responsive layouts to interactive elements,</p>
+
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="col-xl-3 col-md-6 col-sm-12 mt-3">
+                        <div class="card h-100">
+                            <div class="card-body">
+                            
+                                <h5 class="card-title mb-4">
+                                    <i class="bi bi-magic"></i> &nbsp; Admin Tools
+                                </h5>
+
+                                <p>Flow provides a user-friendly admin dashboard that streamlines administrative tasks. With features such as user management, analytics, and customizable reporting</p>
+
+                            </div>
+                        </div>
+                    </div>
+            
+                </div>
+
+            </div>			
+		</div>
+	</div>
+@endsection
