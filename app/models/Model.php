47f74cdc44db71daaca2806f912f2f791e7e3fa4
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-12 21:08:40 +0300

    ✨ Application model Directory

---
+++ app/models/Model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * Base Model
+ * ---
+ * The base model provides a space to set atrributes
+ * that are common to all models
+ */
+class Model extends \Leaf\Model {}
