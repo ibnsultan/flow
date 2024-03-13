6285354d4874d2bfb4b70b109692eb0744ac54bb
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-13 23:18:56 +0300

    ✨ Notifications model

---
+++ app/models/Notifications.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+class Notifications extends Model{
+
+    protected $table = 'notifications';
+
+    protected $fillable = [
+       'title', 'message', 'status', 'user_id'
+    ];
+
+    public $timestamps = true;
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+
+    // select unread, order by date
+    public function user($user){
+        return $this->where('user_id', $user)->where('status', 0)->orderBy('created_at', 'desc')->get();
+    }
+
+    public function markAsRead($id){
+        return $this->where('id', $id)->update(['status' => 1]);
+    }
+
+}
