6fe7a977ca5b0227887b7eebb99f643c61aca1a5
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-16 00:08:59 +0300

    💫 Migrations: Announcement

---
+++ app/database/migrations/2024_03_15_233713_create_announcements.php
@@ -0,0 +1,37 @@
+<?php
+
+use Leaf\Schema;
+use Leaf\Database;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateAnnouncements extends Database
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        if (!static::$capsule::schema()->hasTable('announcements')) :
+            static::$capsule::schema()->create('announcements', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('title', 100);
+                $table->string('description', 300);
+                $table->text('cover')->nullable();
+                $table->string('link', 100)->unique();
+                $table->timestamp('created_at')->default(static::$capsule::raw('CURRENT_TIMESTAMP'));
+                $table->timestamp('updated_at')->default(static::$capsule::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            });
+        endif;
+
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        static::$capsule::schema()->dropIfExists('announcements');
+    }
+}
