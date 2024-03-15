f07c6169ff62e0fcff601ced0e3bd755aeaa9dcd
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-16 00:11:58 +0300

    🌱 Seeds: Settings

---
+++ app/database/seeds/SettingsSeeder.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Database\Seeds;
+
+use App\Models\Settings;
+use App\Database\Factories\SettingFactory;
+use Illuminate\Database\Seeder;
+
+class SettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * @return void
+     */
+    public function run()
+    {
+        // You can directly create db records
+
+        $setting = new Settings();
+        $settings = array(
+            array(
+                "key" => "logo_light",
+                "value" => "/assets/images/brand/logo.png",
+            ),
+            array(
+                "key" => "logo_dark",
+                "value" => "/assets/images/brand/logo.png",
+            ),
+            array(
+                "key" => "favicon",
+                "value" => "/assets/images/brand/icon.png",
+            ),
+            array(
+                "key" => "system_version",
+                "value" => "alpha",
+            ),
+            array(
+                "key" => "default_layout",
+                "value" => "ltr",
+            ),
+            array(
+                "key" => "allow_landing",
+                "value" => "true",
+            ),
+            array(
+                "key" => "allow_blog",
+                "value" => "true",
+            ),
+            array(
+                "key" => "allow_apis",
+                "value" => "true",
+            ),
+        );
+
+        foreach($settings as $entry) {
+            $setting->create($entry);
+        }
+    }
+}
