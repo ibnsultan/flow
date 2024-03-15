78234166c9051b8fcf49ae0c5fdd801053d1c51b
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-15 11:33:45 +0300

    Upload class

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -24,4 +24,49 @@ class Helpers{
         return self::$user_roles[$role];   
     }
 
+    public static function upload(string $fileInput, ?string $storeDir=null, ?array $fileTypes = null, $virtualPath = true ) :array
+    {
+        $fileType = pathinfo($_FILES[$fileInput]['name'], PATHINFO_EXTENSION);
+        $fileName = uniqid() . '.' . $fileType;
+
+        // pass the default directory
+        if (!$storeDir)
+            $storeDir = getcwd() . '/storage/app/uploads/';
+
+        if(!is_dir($storeDir))
+            mkdir($storeDir, 0777, true);
+        
+            
+        // check file validity
+        if ($fileTypes && !in_array($fileType, $fileTypes)) {
+            return [
+                'error' => true,
+                'message' => 'Invalid file type'
+            ];
+        }
+
+        // check if directory exists
+        if (move_uploaded_file($_FILES[$fileInput]['tmp_name'], $storeDir . $fileName)) {
+
+            if($virtualPath)
+                $filePath = str_replace(
+                    [getcwd(), 'storage/app'],
+                    ['', 'storage'],
+                    "/$storeDir$fileName"
+                );
+
+            return [
+                'error' => false,
+                'message' => 'File uploaded successfully',
+                'file' => $fileName,
+                'path' => $filePath
+            ];
+        } else {
+            return [
+                'error' => true,
+                'message' => 'Error uploading file'
+            ];
+        }
+    }
+
 }
