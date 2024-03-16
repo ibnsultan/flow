b6f44d48ce301aa11d7565a62448d64f126c829f
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-16 13:29:00 +0300

    ✨ Starter Template

---
+++ app/views/app/start.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')	
+@section('content')
+	<div class="pc-container">
+		<div class="pc-content">
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header position-relative">
+                            <h5>
+                                Starter Template
+                            </h5>                
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <p>This is your starter template, it's located in <pre class="fw-bold">app/views/app/start.blade.php</pre></p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+@endsection
