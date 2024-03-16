00fc2b2ec548b9bfe60b8abf8521b637a3d7ce79
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-16 13:28:46 +0300

    ✨ APi activity monitor

--- app/views/app/api/activity.blade.php
+++ app/views/app/api/activity.blade.php
@@ -10,7 +10,12 @@
                             <h5>
                                 API Activity/History: &nbsp;
                                 <span class="text-primary">{{$apiKey->name}}</span>
-                            </h5>                
+                            </h5>
+                            <button class="btn btn-primary position-absolute" 
+                                style="top:0.8rem;right:2rem;" 
+                                 data-bs-toggle="modal" data-bs-target="#copyApiKey">
+                                Copy Key
+                            </button>             
                         </div>
                     </div>
                 </div>
@@ -21,9 +26,41 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                            <div class="table table-responsive">
-                            
-                            </div>
+                            @if(count($activities) > 0)
+                                <div class="table table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th></th>
+                                                <th>{{_('Handler')}}</th>
+                                                <th>{{_('Origin')}}</th>
+                                                <th class="w-25">{{_('Payload')}}</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach($activities as $activity)
+                                                <tr>
+                                                    <td style="font-size: 10px;">
+                                                        {!! ($activity->status == 'pass') ? 
+                                                            '<span class="badge bg-success">pass</span>' : 
+                                                            '<span class="badge bg-danger">fail</span>' 
+                                                        !!}
+                                                    </td>
+                                                    <td>{{$activity->handler}}</td>
+                                                    <td>{{$activity->origin}}</td>
+                                                    <td><code>{{ json_encode($activity->payload, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)}}<code></td>
+                                                    <td class="text-end">{{$activity->created_at}}</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            @else
+                                <div class="text-center">
+                                    No activity recorded for this API key
+                                </div>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -32,4 +69,70 @@
 
         </div>
     </div>
+
+    <div class="modal fade" id="copyApiKey" tabindex="-1" aria-labelledby="copyApiKeyLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <form name="copyApiKeyForm" method='post'>
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="copyApiKeyLabel">Copy API Key</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label class="mb-1">Secret Key</label>
+                            <input type="text" class="form-control" name="api_secret" placeholder="Enter the API secret key" required>
+                            <input type="text" name="api_id" value="{{$apiID}}" hidden>
+                        </div>
+
+                        <div class="form-group position-relative">
+                            <textarea id="keyPreview" class="form-control" rows="4" onclick="copyKey()" readonly>{{ \App\Helpers\Helpers::previewKey($apiKey->token) }}</textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type='submit' class="btn btn-primary">Get API Key</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        $("form[name='copyApiKeyForm']").submit(function(event) {
+            event.preventDefault();
+            
+            $.ajax({
+                url: '/app/api/copy',
+                type: 'POST',
+                data: $(this).serialize(),
+                success: function(response) {
+                    if(response.status == 'success') {
+                        
+                        var key = response.message;
+                        
+                        $('#keyPreview').val(key)
+                        copyKey();
+                        
+                    } else {
+                        Swal.fire({ icon: 'error', title: 'Oops...', text: response.message })
+                    }
+                },
+                error: function() {
+                    Swal.fire({ icon: 'error', title: 'Oops...', text: 'An error occurred while processing your request' })
+                }
+            });
+        });
+
+        function copyKey() {
+            var keyPreview = document.getElementById('keyPreview');
+            keyPreview.select();
+            document.execCommand('copy');
+
+            
+            Swal.fire({ icon: 'success', title: 'Success', text: 'API key copied to clipboard' })
+        }
+    </script>
 @endsection
