3bd33389a4ef79d36de2386befa67c2d2eae450c
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-17 23:20:05 +0300

    ✨ Admin, Blog routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,6 +3,7 @@
 app()->get('/', fn() => render('welcome'));
 
 
+# Application routes
 app()->group('app', function(){
 
     app()->get('home', 'App\HomeController@home');
@@ -19,6 +20,7 @@ app()->group('app', function(){
     });
     
 
+    # API routes
     app()->group('api', function(){
 
         app()->get('manage', 'App\ApiController@index');
@@ -30,4 +32,46 @@ app()->group('app', function(){
     });
 
 
+    # Blog routes
+    app()->group('blog', function(){
+
+        app()->get('/', 'App\BlogController@index');
+        app()->get('article/{id}', 'App\BlogController@article');
+        app()->get('category/{id}', 'App\BlogController@category');
+
+    });
+
+    app()->group('media', function(){
+
+        app()->post('upload', 'MediaController@upload');
+
+    });
+
+
+});
+
+
+# Admin routes
+app()->group('admin', function(){
+    
+    app()->get('', 'Admin\DashboardController@home');
+    
+    # Blog Routes
+    app()->group('blog', function(){
+        
+        # articles routes
+        app()->get('/', 'Admin\BlogController@index');
+        app()->get('article/write', 'Admin\BlogController@writeArticle');
+
+        app()->post('article/create', 'Admin\BlogController@createArticle');
+        app()->post('article/update', 'Admin\BlogController@updateArticle');
+
+        # article categories routes
+        app()->get('categories', 'Admin\BlogController@categories');
+        app()->get('categories/{id}/delete', 'Admin\BlogController@deleteCategory');
+
+        app()->post('category/create', 'Admin\BlogController@createCategory');
+        app()->post('category/update', 'Admin\BlogController@updateCategory');
+        
+    });
 });
