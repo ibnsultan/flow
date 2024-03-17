323037815a8c28216154f8890d0a027c4eab7981
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-17 23:20:42 +0300

    ✨ Articles listing

---
+++ app/views/admin/blog/index.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.admin')	
+@section('content')    
+	<div class="pc-container">
+		<div class="pc-content">
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body position-relative">
+                            <h5>
+                                {{_('Posts')}}
+                            </h5> 
+                            <a class="btn btn-primary position-absolute" 
+                                style="top:0.8rem;right:2rem;" href="/admin/blog/article/write">
+                                {{_('Add New')}}
+                            </a>              
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+            
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            @if(count($articles) > 0)
+                                <div class="table-responsive">
+                                    <table class="table table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>{{_('Title')}}</th>
+                                                <th>{{_('Author')}}</th>
+                                                <th>{{_('Category')}}</th>
+                                                <th>{{_('Published')}}</th>
+                                                <th>{{_('Actions')}}</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            
+                                            @foreach ($articles as $article)
+
+                                                <tr>
+                                                    <td>
+                                                        <a href="/admin/blog/{{ \App\Helpers\Helpers::encode($article->id) }}">
+                                                            {{ $article->title }}
+                                                        </a>
+                                                    </td>
+                                                    <td>
+                                                        {{ $article->users->fullname }}
+                                                    </td>
+                                                    <td>
+                                                        {{ $article->blog_categories->name }}
+                                                    </td>
+                                                    <td>
+                                                        {{ $article->created_at->diffForHumans() }}
+                                                    </td>
+                                                    <td>
+                                                        <a href="/admin/blog/{{ $article->id }}/edit" class="btn btn-primary rounded btn-sm">
+                                                            <i class="bi bi-pencil"></i>
+                                                        </a>
+                                                        <a href="/admin/blog/{{ $article->id }}/delete" class="btn btn-danger rounded btn-sm">
+                                                            <i class="bi bi-trash"></i>
+                                                        </a>
+                                                    </td>
+
+                                            @endforeach
+                                            
+                                        </tbody>
+                                    </table>
+                                </div>
+                            @else
+                                <div class="text-center">
+                                    <h5>{{_('No posts found')}}</h5>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+@endsection
