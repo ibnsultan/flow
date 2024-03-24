f22142ba2b145454206d61ad45397db6a5dd4b10
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-24 22:03:47 +0300

    Email starter template

---
+++ app/views/mail/template.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.mail')
+@section('content')
+
+	<!--span class="preheader">This is preheader text. Some clients will show this text as a preview.</span-->
+	<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main">
+		<!-- START MAIN CONTENT AREA -->
+		<tr>
+			<td style="text-align:center; padding-top:1rem;"> 
+				<img width="200" src="{{settings->get('logo_dark')}}" alt="Logo">
+			</td>
+		</tr>
+		<tr>
+			<td class="wrapper">
+				<p>Hi there</p>
+				<p>Sometimes you just want to send a simple HTML email with a simple design and clear call to action. This is it.</p>
+				<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+					<tbody>
+						<tr style="text-align:center">
+							<td> <a href="http://htmlemail.io" target="_blank">Call To Action</a> </td>
+						</tr>
+					</tbody>
+				</table>
+				<p>This is a really simple email template. It's sole purpose is to get the recipient to click the button with no distractions.</p>
+				<p>Good luck! Hope it works.</p>
+			</td>
+		</tr>
+		<!-- END MAIN CONTENT AREA -->
+	</table>				
+
+@endsection
