5b30445e0552bb0be825cdc95992c57a580e7acb
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-24 22:04:01 +0300

    User onboarding email

--- app/views/mail/welcome.blade.php
+++ app/views/mail/welcome.blade.php
@@ -1,30 +1,29 @@
 @extends('layouts.mail')
 @section('content')
 
-	<!--span class="preheader">This is preheader text. Some clients will show this text as a preview.</span-->
-	<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main">
-		<!-- START MAIN CONTENT AREA -->
-		<tr>
-			<td style="text-align:center; padding-top:1rem;"> 
-				<img width="200" src="{{settings->get('logo_dark')}}" alt="Logo">
-			</td>
-		</tr>
-		<tr>
-			<td class="wrapper">
-				<p>Hi there</p>
-				<p>Sometimes you just want to send a simple HTML email with a simple design and clear call to action. This is it.</p>
-				<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
-					<tbody>
-						<tr style="text-align:center">
-							<td> <a href="http://htmlemail.io" target="_blank">Call To Action</a> </td>
-						</tr>
-					</tbody>
-				</table>
-				<p>This is a really simple email template. It's sole purpose is to get the recipient to click the button with no distractions.</p>
-				<p>Good luck! Hope it works.</p>
-			</td>
-		</tr>
-		<!-- END MAIN CONTENT AREA -->
-	</table>				
+    <!--span class="preheader">This is preheader text. Some clients will show this text as a preview.</span-->
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main">
+        <!-- START MAIN CONTENT AREA -->
+        <tr>
+            <td style="text-align:center; padding-top:1rem;">
+                <img width="200" src="{{getenv('app_url')}}{{ settings->get('logo_dark') }}" alt="Logo">
+            </td>
+        </tr>
+        <tr>
+            <td class="wrapper">
+                <p>Hi {{ $name }},</p>
+                <p>Your account has been successfully created by an administrator. Below are your login credentials:</p>
+                <p><strong>Username:</strong> {{ $username }}</p>
+                <p><strong>Password:</strong> {{ $password }}</p>
+                <p>For security reasons, we recommend changing your password after your first login.</p>
+                <p>You can log in to your account <a href="{{ $loginLink }}" target="_blank">here</a>.</p>
+                <p>If you have any questions or need further assistance, feel free to contact us.</p>
+                <p>Welcome aboard!</p>
+                <p>Best regards,</p>
+                <p>{{ getenv('app_name') }}</p>
+            </td>
+        </tr>
+        <!-- END MAIN CONTENT AREA -->
+    </table>
 
 @endsection
