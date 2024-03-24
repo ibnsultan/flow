11f56dcf0cc182ea00861a164279e46dd7833417
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-24 19:16:31 +0300

    password reset routes

--- app/routes/auth.php
+++ app/routes/auth.php
@@ -9,12 +9,15 @@ app()->redirect('/register', '/auth/register');
 app()->group('auth', function(){
 
     app()->get('/login', 'AuthController@signin');
-    app()->get('/reset', 'AuthController@reset');
+    app()->get('/reset', 'AuthController@forgot');
     app()->get('/register', 'AuthController@signup');
 
-    app()->post('login', 'AuthController@login');
-    app()->post('reset', 'AuthController@reset');
-    app()->post('register', 'AuthController@register');
+    app()->post('/login', 'AuthController@login');
+    app()->post('/reset', 'AuthController@reset');
+    app()->post('/register', 'AuthController@register');
+
+    app()->get('/password/{token}', 'AuthController@changePassword');
+    app()->post('/password/{token}', 'AuthController@updatePassword');
 
     app()->get('/logout', 'AuthController@logout');
 
@@ -22,7 +25,7 @@ app()->group('auth', function(){
 
 app()->group('api', function(){
 
-    app()->group('auth', function(){
+    app()->group('/auth', function(){
 
         app()->post('/login', fn() => AuthController::login(true));
 
