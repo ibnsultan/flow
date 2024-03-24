6a8bf8b5d117fda2004b0c9b2943ed1fd7cd0842
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-24 22:03:16 +0300

    Create new user (from admin)

--- app/views/admin/users/index.blade.php
+++ app/views/admin/users/index.blade.php
@@ -11,7 +11,8 @@
                                 {{_('Users')}}
                             </h5> 
                             <a class="btn btn-primary position-absolute" 
-                                style="top:0.8rem;right:2rem;" href="/admin/users/add">
+                                style="top:0.8rem;right:2rem;" href="/admin/users/add"
+                                data-bs-toggle="modal" data-bs-target="#registerNewUser">
                                 {{_('Add New')}}
                             </a>              
                         </div>
@@ -77,4 +78,96 @@
 
         </div>
     </div>
+
+    <!-- User Registration Modal -->
+    <div class="modal fade" id="registerNewUser" tabindex="-1" aria-labelledby="registerNewUserLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="registerNewUserLabel">{{_('Register New User')}}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                    <div class="alert bg-light-warning text-warning" role="alert">
+                        {{_('User registered by admin will automatically be active')}}
+                    </div>
+
+                    <form action="post" name="userRegistrationForm">
+
+                        <div class="row">
+                            <div class="col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <label class="form-label">Full Name</label>
+                                    <input type="text" name="fullname" class='form-control' placeholder="John Doe" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <label class="form-label">Email address</label>
+                                    <input type="text" name="email" class='form-control' placeholder="doe@example.com" required>
+                                </div>
+                            </div>
+                            <div class="col-md-12 col-sm-12">
+                                <div class="form-group">
+                                    <label class="form-label">User role</label>
+                                    <select name="user_role" class="form-select">
+                                        <option value="subscriber" hidden>{{_('Subscriber')}}</option>
+                                        @foreach (\App\Models\UserRole::all() as $role)
+                                            <option value="{{ $role->name }}">{{ $role->name }}</option>                                                        
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-12 col-sm-12">
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary rounded w-100 ">{{_('Create User')}}</button>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('scripts')
+    <script>
+        $('form[name="userRegistrationForm"]').submit(function(e){
+
+			e.preventDefault();
+			
+			// disable button to prevent multiple clicks
+			$('button[type="submit"]').attr('disabled', true).html('Processing...');
+
+			$.ajax({
+				url: '/admin/user/create',
+				type: 'POST',
+				data: $(this).serialize(),
+				success: function(response) {
+					if(response.status == 'success') {
+
+						Swal.fire({ icon: 'success', title: 'Success', text: response.message }).then(() => {
+							location.reload();
+						});
+
+					} else {
+
+						Swal.fire({ icon: 'error', title: 'Oops...', text: response.message });
+						$('button[type="submit"]').attr('disabled', false).html('Create User');
+
+					}
+				},
+				error: function() {
+
+					Swal.fire({ icon: 'error', title: 'Oops...', text: 'An error occurred. Please try again later.' });
+					$('button[type="submit"]').attr('disabled', false).html('Create User');
+
+				}
+			});
+
+		})
+    </script>
 @endsection
