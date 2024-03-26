ec42830a516afacbdf9ee19a906cf8c5ad2ff5e2
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-26 23:39:17 +0300

    Languages migration table

---
+++ app/database/migrations/2024_03_26_232315_create_languages.php
@@ -0,0 +1,36 @@
+<?php
+
+use Leaf\Schema;
+use Leaf\Database;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateLanguages extends Database
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        if (!static::$capsule::schema()->hasTable('languages')) :
+            static::$capsule::schema()->create('languages', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name', 50);
+                $table->char('iso', 50);
+                $table->char('layout', 50);
+                $table->char('status', 50);
+                $table->timestamp('created_at')->default(static::$capsule::raw('CURRENT_TIMESTAMP'));
+                $table->timestamp('updated_at')->default(static::$capsule::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));                
+            });
+        endif;
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        static::$capsule::schema()->dropIfExists('languages');
+    }
+}
