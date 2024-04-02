4f7face3c7abee09a3b2461ed3deebaa134c22aa
ibnsultan <abdulbasitsultan4@gmail.com>
2024-04-02 21:05:38 +0300

    Added deleteuser and update user methods

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers\Admin;
 
+use App\Helpers\Helpers;
 use Leaf\Helpers\Password;
 use App\Helpers\MailSender;
 
@@ -31,7 +32,8 @@ class UserController extends Controller
 
         $data = [
             'title' => 'Users',
-            'users' => $users
+            'users' => $users,
+            'helper' => new Helpers,
         ];
 
         response()->markup(view('admin.users.index', $data));
@@ -92,15 +94,81 @@ class UserController extends Controller
      */
     public function viewUser($id){
 
-        $user_id = \App\Helpers\Helpers::decode($id);
-        if($user_id == '') exit(response()->page(getcwd()."/app/views/errors/404.html"));
+        $user_id = Helpers::decode($id);
+        if($user_id == '')
+            exit(response()->page(getcwd()."/app/views/errors/404.html"));
 
         $data = [
             'title' => "Manage: {$this->users->find($user_id)->fullname}",
-            'user' => $this->users->getUser($user_id)
+            'user' => $this->users->getUser($user_id),
+            'helper' => new Helpers
         ];
 
         response()->markup( view('admin.users.view', $data) );
 
+    }
+
+    /**
+     * Delete User
+     * 
+     * @param string $id
+     * @return void
+     */
+    public function deleteUser($id){
+
+        $user_id = Helpers::decode($id);
+        if($user_id == '')
+            exit(response()->page(getcwd()."/app/views/errors/404.html"));
+
+        if($this->users->find($user_id)->role == 'admin')
+            exit(response()->json(['status'=>'error', 'message'=>'You cannot delete an admin']));
+
+        $this->users->find($user_id)->delete();
+        response()->json(['status'=>'success', 'message'=>'User deleted successfully']);
+
+    }
+
+
+    /**
+     * Update User
+     * 
+     * @param string $id
+     * @return void
+     */
+
+    public function updateUser(){
+
+        $user_id = Helpers::decode(request()->get('user_id'));
+        if($user_id == '')
+            exit(response()->page(getcwd()."/app/views/errors/404.html"));
+
+        $user = $this->users->find($user_id);
+
+        // check if user exists
+        if(!$user)
+            exit(response()->json(['status'=>'error', 'message'=>'User not found']));
+
+        try {
+
+            (request()->get('phone') == '') ? $phone = null : $phone = request()->get('phone');
+
+            // update user records
+            $user->update([
+                'fullname' => request()->get('fullname'),
+                'email' => request()->get('email'),
+                'phone' => $phone,
+                'role' => request()->get('user_role'),
+                'status' => request()->get('status'),
+                'about' => request()->get('bio')
+            ]);
+
+            response()->json(['status'=>'success', 'message'=>'User updated successfully']);
+
+        } catch (\Throwable $e) {
+            
+            (getenv('app_debug') == 'true') ?
+                response()->json(['status'=>'error', 'message'=>$e->getMessage()]) :
+                response()->json(['status'=>'error', 'message'=>'An error occurred']);
+        }
     }
 }
