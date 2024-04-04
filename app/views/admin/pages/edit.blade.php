0758259e54c62829ee415c2c3708e7316ae7d143
ibnsultan <abdulbasitsultan4@gmail.com>
2024-04-04 20:57:17 +0300

    Migrated infile js to admin.js

--- app/views/admin/pages/edit.blade.php
+++ app/views/admin/pages/edit.blade.php
@@ -4,10 +4,11 @@
     <div class="pc-container">
         <div class="pc-content">
             
-            <form action="post" enctype="multipart/form-data" name="addPage">
+            <form action="post" enctype="multipart/form-data" name="updatePage">
                 <div class="row">
                     <div class="col-md-8 col-sm-12">
                         <div class="card h-100">
+                            <input type="hidden" id="pageId" name="id" value="{{ $helper::encode($page->id) }}">
                             <div class="card-body editor-board">
 
                                 <textarea name="content" id="classic-editor" required hidden> {!! $page->content !!} </textarea>
@@ -62,58 +63,3 @@
     </div>
 
 @endsection
-@section('scripts')
-    <script src="/assets/js/plugins/ckeditor/classic/ckeditor.js"></script>
-    
-    <script>
-        ClassicEditor
-            .create( document.querySelector( '#classic-editor' ), {} )
-            .catch( error => {console.error( error )
-        });
-        
-        // TODO: listens to ckeditor changes instead of interval
-        setInterval(function() {
-            var ckEditable = document.querySelector('.ck-editor__editable').innerHTML;
-
-            if(ckEditable != $('#classic-editor').val()) {
-                $('#classic-editor').val(ckEditable);
-            }
-        }, 1000);
-
-        // slugify title
-        document.forms.addPage.title.addEventListener('keyup', function() {
-            var title = this.value;
-            var slug = title.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
-            document.forms.addPage.slug.value = slug;
-        });
-
-        // on submit form
-        document.forms.addPage.addEventListener('submit', function(e) {
-            e.preventDefault();
-            
-            $.ajax({
-                url: '/admin/pages/update/{{ \App\Helpers\Helpers::encode($page->id) }}',
-                type: 'post',
-                data: new FormData(this),
-                contentType: false,
-                cache: false,
-                processData: false,
-                success: function(response) {
-                    if(response.status == 'success') {
-                        Swal.fire( 'Success!', response.message, 'success' ).then((result) => {
-                            location.href = '/admin/pages/'
-                        });
-                    }else{
-                        Swal.fire( 'Error!', response.message, 'error' );
-                    }
-                },
-                error: function(response) {
-                    Swal.fire( 'Error!', 'The article could not be published.', 'error' );
-                }
-            });
-
-        });
-
-
-    </script>
-@endsection 
