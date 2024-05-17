37298bb13630300fc0d83ff24e7124065a43e0ca
ibnsultan <abdulbasitsultan4@gmail.com>
2024-05-18 00:47:59 +0300

    Modules Migration

---
+++ app/database/migrations/2024_05_17_185910_create_modules.php
@@ -0,0 +1,35 @@
+<?php
+
+use Leaf\Schema;
+use Leaf\Database;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateModules extends Database
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        if (!static::$capsule::schema()->hasTable('modules')) :
+            static::$capsule::schema()->create('modules', function (Blueprint $table) {
+                $table->id();
+                $table->string('module_name');
+                $table->string('description')->nullable();
+                $table->string('status')->default('active');
+                $table->timestamp('created_at')->default(static::$capsule::raw('CURRENT_TIMESTAMP'));
+                $table->timestamp('updated_at')->default(static::$capsule::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            });
+        endif;
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        static::$capsule::schema()->dropIfExists('modules');
+    }
+}
