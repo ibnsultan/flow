03af0948dc12b011a37c3f16ff9ee05904621ad5
ibnsultan <abdulbasitsultan4@gmail.com>
2024-05-18 00:48:49 +0300

    Roles table migration

---
+++ app/database/migrations/2024_05_17_195314_create_roles.php
@@ -0,0 +1,34 @@
+<?php
+
+use Leaf\Schema;
+use Leaf\Database;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateRoles extends Database
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        if (!static::$capsule::schema()->hasTable('roles')) :
+            static::$capsule::schema()->create('roles', function (Blueprint $table) {
+                $table->id();
+                $table->string('name', 50);
+                $table->text('description')->nullable();
+                $table->timestamp('created_at')->default(static::$capsule::raw('CURRENT_TIMESTAMP'));
+                $table->timestamp('updated_at')->default(static::$capsule::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            });
+        endif;
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        static::$capsule::schema()->dropIfExists('roles');
+    }
+}
