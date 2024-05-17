c7cfeb62dda1e6c9df7ce9ae0b2fabc5dfc56169
ibnsultan <abdulbasitsultan4@gmail.com>
2024-05-18 00:50:15 +0300

    Roles seeder

---
+++ app/database/seeds/RolesSeeder.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Database\Seeds;
+
+use App\Models\Role;
+use App\Database\Factories\RoleFactory;
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * @return void
+     */
+    public function run()
+    {
+        // admin
+        Role::create([
+            "role_name" => "admin",
+            "description" => "Admin role"
+        ]);
+    }
+}
