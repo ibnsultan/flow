724da19bc86d65b51408ef94b41d24651a1176be
ibnsultan <abdulbasitsultan4@gmail.com>
2024-06-27 12:36:36 +0300

    Fetch Permissions

--- app/models/RolePermission.php
+++ app/models/RolePermission.php
@@ -16,6 +16,39 @@ class RolePermission extends Model
         "updated_at" => "datetime"
     ];
 
+    public static function byRole(int $roleId) :mixed
+    {
+        return db()->query("SELECT m.name AS 'module', p.name AS 'permission', t.name AS 'scope' 
+            FROM role_permissions rp, roles r, permission_types t, permissions p, modules m
+            WHERE rp.role_id = r.id AND rp.permission_id = p.id AND rp.permission_type_id = t.id AND p.module_id = m.id AND rp.role_id = $roleId")
+            ->get();
+    }
+
+    public static function byPermission(int $permissionId, int $roleId) :mixed
+    {
+        return db()->query("SELECT m.name AS 'module', p.name AS 'permission', t.name AS 'scope' 
+            FROM role_permissions rp, roles r, permission_types t, permissions p, modules m
+            WHERE rp.role_id = r.id AND rp.permission_id = p.id AND rp.permission_type_id = t.id AND p.module_id = m.id AND 
+                  rp.permission_id = $permissionId AND rp.role_id = $roleId")
+            ->get();
+    }
+
+    public static function updateExisting(int $roleId, int $permissionId, int $permissionTypeId) :bool
+    {
+        $permission = self::where("role_id", $roleId)->where("permission_id", $permissionId)->first();
+        
+        # delete is $permissionTypeId is 0, else update
+        if($permission):
+            if($permissionTypeId == 0) return $permission->delete();
+            
+            $permission->permission_type_id = $permissionTypeId;
+            return $permission->save();
+
+            else: return false;
+        endif;
+
+    }
+
     public function permission()
     {
         return $this->belongsTo(Permission::class);
