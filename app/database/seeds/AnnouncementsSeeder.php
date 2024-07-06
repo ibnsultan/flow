b920b121ee9bfb41f4d6ff96476755fe9edc662f
ibnsultan <abdulbasitsultan4@gmail.com>
2024-07-06 13:21:30 +0300

    Announcement Seed

---
+++ app/database/seeds/AnnouncementsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Database\Seeds;
+
+use App\Models\Announcement;
+use Illuminate\Database\Seeder;
+
+class AnnouncementsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * @return void
+     */
+    public function run()
+    {
+        Announcement::create([
+            'title' => 'Welcome to Flow!',
+            'description' => 'This is the first announcement on Flow. Welcome!',
+            'recipients' => ['admin', 'user'],
+            'link' => 'https://github.com/ibnsultan/flow'  
+        ]);
+    }
+}
