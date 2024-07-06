c356ae4a8fc7f5cd45a42f90172532d1b77039ec
ibnsultan <abdulbasitsultan4@gmail.com>
2024-07-06 13:20:50 +0300

    Announcement Controller

--- app/controllers/Admin/AnnouncementController.php
+++ app/controllers/Admin/AnnouncementController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controllers\Admin;
 
+use App\Helpers\Helpers;
+use App\Middleware\PermissionHandler;
+
+use App\Models\Role;
 use App\Models\Announcement;
 use App\Controllers\Controller;
 
@@ -10,10 +14,68 @@ class AnnouncementController extends Controller
     public function index() :void
     {
 
+        $this->data->roles = Role::all();
         $this->data->title = 'Announcement List';
         $this->data->announcements = Announcement::all();
 
         render('admin.announcement.index', (array) $this->data);
 
+    }
+
+    public function add() :void
+    {
+        if(!PermissionHandler::can('setting', 'add_announcements')->status)
+            exit(response()->json(['status' => 'error', 'message' => __('You do not have permission to add announcements')]));
+        
+        try{
+            $data['title'] = request()->params('title');
+            $data['description'] = request()->params('description');
+            $data['recipients'] = request()->params('recipients');
+
+            if(in_array(null, $data)) 
+                exit(response()->json(['status' => 'error', 'message' => 'Title, description and recipients are required']));
+
+            /*if(request()->get('cover')['size']){
+                $file = Helpers::upload(
+                    'cover',
+                    'storage/app/uploads/announcements/',
+                    ['jpg', 'jpeg', 'png', 'gif']
+                );
+
+                if($file['error']) exit( response()->json(['status' => 'error', 'message' => $file['message']]) );
+
+                $data['cover'] = $file['path'];
+            }*/
+
+            $data['link'] = request()->params('link');
+
+            Announcement::create($data);
+
+            response()->json(['status' => 'success', 'message' => __('Announcement created successfully')]);
+            
+        }catch(\Exception $e){
+            response()->json(['status' => 'error', 'message' => __('An error occurred while creating announcement'),
+                'debug' => [
+                    'message' => $e->getMessage(),
+                    'line' => $e->getLine() . ' in ' . $e->getFile()
+                ]
+            ]);
+        }
+    }
+
+    public function delete($id) :void
+    {
+
+        if(!PermissionHandler::can('settings', 'delete_announscements'))
+            exit(response()->json(['status' => 'error', 'message' => __('You do not have permission to delete announcements')]));
+
+        $announcement = Announcement::find($id);
+
+        if(!$announcement)
+            exit(response()->json(['status' => 'error', 'message' => __('Announcement not found')]));
+
+        $announcement->delete();
+
+        response()->json(['status' => 'success', 'message' => __('Announcement deleted successfully')]);
     }
 }
