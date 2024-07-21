b2ee703fbb6947ec8c173c832edea5c976eb7a35
ibnsultan <abdulbasitsultan4@gmail.com>
2024-07-22 01:34:26 +0300

    Fix: renamed col `module_name` -> `name`, added col `is_core`

--- app/database/migrations/2024_03_01_000001_create_modules.php
+++ app/database/migrations/2024_03_01_000001_create_modules.php
@@ -15,9 +15,10 @@ class CreateModules extends Database
         if (!static::$capsule::schema()->hasTable('modules')) :
             static::$capsule::schema()->create('modules', function (Blueprint $table) {
                 $table->id();
-                $table->string('module_name');
+                $table->string('name');
                 $table->string('description')->nullable();
                 $table->string('status')->default('active');
+                $table->integer('is_core')->default(0);
                 $table->timestamp('created_at')->default(static::$capsule::raw('CURRENT_TIMESTAMP'));
                 $table->timestamp('updated_at')->default(static::$capsule::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             });
