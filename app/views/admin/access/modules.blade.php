d58cb483b866b32ac1690121c0badd81cd3a42fe
ibnsultan <abdulbasitsultan4@gmail.com>
2024-07-22 00:41:15 +0300

    Update: Module creation form, minor layout fixes

--- app/views/admin/access/modules.blade.php
+++ app/views/admin/access/modules.blade.php
@@ -10,10 +10,12 @@
                             <h5>
                                 {{ __($title) }}
                             </h5>
-                            <a class="btn btn-primary rounded position-absolute" style="top:0.8rem;right:2rem;"
-                                data-bs-toggle="modal" data-bs-target="#addNewModule">
-                                {{__('Add New')}}
-                            </a>              
+                            @if($addModulePermission->status)
+                                <a class="btn btn-primary rounded position-absolute" style="top:0.8rem;right:2rem;"
+                                    data-bs-toggle="modal" data-bs-target="#addNewModule">
+                                    {{__('Add New')}}
+                                </a>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -31,10 +33,10 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card card-body">
-                        @if($modules->count() > 0)
+                        @if($appModules->count() > 0)
                             <div class="row">
                                 
-                                @foreach($modules as $module)
+                                @foreach($appModules as $module)
                                     <div class="col-md-4 col-sm-12">
                                         <div class="card mb-3">
                                             <div class="card-body position-relative">
@@ -44,7 +46,7 @@
                                                 <!-- switch: 'active'|'inactive' -->
                                                 <div class="form-check form-switch position-absolute" style="top:1.6rem; right: 1rem;">
                                                     <input class="form-check form-switch module-switch" type="checkbox"
-                                                        {{ $module->is_core ? 'disabled' : 'data-module='.$helpers::encode($module->id) }}
+                                                        {{ (!$module->is_core and $editModulePermission->status) ? 'data-module='.$helpers::encode($module->id) : 'disabled' }}
                                                         {{ $module->status  ? 'checked' : '' }}>
                                                 </div>
                                             </div>
@@ -69,67 +71,73 @@
     </div>
 
     <!-- modal: new user role -->
-    <div class="modal fade" id="addNewModule" tabindex="-1" aria-labelledby="addNewModuleLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="addNewModuleLabel"> {{__('Register a Module')}} </h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form name="addNewModule" method="post">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label class="form-label"> {{__('Name')}} </label>
-                                    <input type="text" name="name" class="form-control" required>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label class="form-label"> {{__('Description')}} </label>
-                                    <textarea name="description" class="form-control" rows="3" required></textarea>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label class="form-label"> {{__('Primary Permissions')}} </label> <br>
-                                    <!-- inline checkboxes: all, added, owned, both, none -->
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" value="create" name="permissions[]">
-                                        <label class="form-check-label" for="inlineCheckbox1"> {{__('Create')}} </label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" value="read" name="permissions[]">
-                                        <label class="form-check-label" for="inlineCheckbox2"> {{__('Read')}} </label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" value="update" name="permissions[]">
-                                        <label class="form-check-label" for="inlineCheckbox3"> {{__('Update')}} </label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" value="delete" name="permissions[]">
-                                        <label class="form-check-label" for="inlineCheckbox2"> {{__('Delete')}} </label>
+    @if($addModulePermission->status)
+        <div class="modal fade" id="addNewModule" tabindex="-1" aria-labelledby="addNewModuleLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addNewModuleLabel"> {{__('Register a Module')}} </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form name="addNewModule" id="formAddModule" method="post">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label class="form-label"> {{__('Name')}} </label>
+                                        <input type="text" name="name" class="form-control" placeholder="{{__('Module Name')}}" required>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-primary w-100"> {{__('Save')}} </button>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label class="form-label"> {{__('Description')}} </label>
+                                        <input type="text" name="description" placeholder="{{__('Module Description')}}" class="form-control" >
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </form>
+                            <div class="row">
+                                <div class="col-12">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th>Scope</th>
+                                                <th>C</th>
+                                                <th>R</th>
+                                                <th>U</th>
+                                                <th>D</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach (['all', 'owned', 'added', 'both', 'none'] as $scope)
+                                                <tr>
+                                                    <td> {{ ucfirst($scope) }} </td>
+                                                    @foreach (['create', 'read', 'update', 'delete'] as $crud)
+                                                        <td>
+                                                            <div class="form-check form-check-inline">
+                                                                <input class="form-check-input" type="checkbox" name="{{$crud}}[]" value="{{$scope}}">
+                                                            </div>
+                                                        </td>
+                                                    @endforeach
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <button type="submit" id="btnAddModule" class="btn btn-primary w-100"> {{__('Save Module')}} </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endif
 @endsection
 @section('scripts')
     <script>
@@ -143,28 +151,52 @@
             });
 
             // module switch
-            $('.module-switch').on('change', function() {
-                var module = $(this).data('module');
-                var status = $(this).prop('checked') ? 1 : 0;
-                var url = "/admin/access/modules/status";
-                var data = {
-                    module: module,
-                    status: status
-                };
-                $.post(url, data, function(response) {
-                    if(response.status === 'success') {
-                        iziToast.success({
-                            message: response.message,
-                            position: 'bottomCenter'
-                        });
-                    } else {
-                        iziToast.error({
-                            message: response.message,
-                            position: 'bottomCenter'
-                        });
-                    }
+            @if($editModulePermission->status)
+                $('.module-switch').on('change', function() {
+                    var module = $(this).data('module');
+                    var status = $(this).prop('checked') ? 1 : 0;
+                    var url = "/admin/access/modules/status";
+                    var data = {
+                        module: module,
+                        status: status
+                    };
+                    $.post(url, data, function(response) {
+                        if(response.status === 'success') {
+                            toast.success({ message: response.message, position: 'bottomCenter' });
+                        } else {
+                            toast.error({ message: response.message, position: 'bottomCenter' });
+                        }
+                    });
                 });
-            });
+            @endif
+
+            // add new module
+            @if($addModulePermission->status)
+                $('#formAddModule').on('submit', function(e) {
+                    e.preventDefault();
+                    buttonState('#btnAddModule', 'loading');
+
+                    $.ajax({
+                        url: '/admin/access/modules/add',
+                        type: 'post',
+                        data: $(this).serialize(),
+                        success: function(response) {
+                            if(response.status === 'success') {
+                                toast.success({ message: response.message, position: 'bottomCenter' });
+                                setTimeout(() => { location.reload(); }, 1000);
+                            } else {
+                                toast.error({ message: response.message, position: 'bottomCenter' });
+                            }
+                        },
+                        error: function(xhr, status, error) {
+                            toast.error({ message: '{{__('An Unkown error Occured')}}', position: 'bottomCenter' });
+                        },
+                        complete: function() {
+                            buttonState('#btnAddModule', 'reset', '{{__('Save Module')}}');
+                        }
+                    });
+                });
+            @endif
         });
     </script>
 @endsection
