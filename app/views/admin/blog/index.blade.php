c95bd3f817a3d44a0500a191cee26ad57e8fd311
ibnsultan <abdulbasitsultan4@gmail.com>
2024-07-30 10:55:33 +0300

    Mod: Permission fixes and validation

--- app/views/admin/blog/index.blade.php
+++ app/views/admin/blog/index.blade.php
@@ -6,82 +6,33 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card">
-                        <div class="card-body position-relative">
-                            <h5>
-                                {{__('Posts')}}
-                            </h5> 
-                            <a class="btn btn-primary position-absolute" 
-                                style="top:0.8rem;right:2rem;" href="/admin/blog/article/write">
-                                {{__('Add New')}}
-                            </a>              
+                        <div class="card-header border-0 position-relative">
+                            <h5>{{ __('Posts') }}</h5>
+                            @if($addArticlePermission->status)
+                                <a class="btn btn-primary position-absolute" 
+                                    style="top:0.8rem;right:2rem;" href="/admin/blog/article/write">
+                                    {{ __('Add New Article') }}
+                                </a>
+                            @endif        
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="row">
-            
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
                             @if(count($articles) > 0)
-                                <div class="table-responsive">
-                                    <table class="table table-hover">
-                                        <thead>
-                                            <tr>
-                                                <th class="w-50">{{__('Title')}}</th>
-                                                <th>{{__('Author')}}</th>
-                                                <th>{{__('Category')}}</th>
-                                                <th>{{__('Published')}}</th>
-                                                <th>{{__('Actions')}}</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            
-                                            @foreach ($articles as $article)
-
-                                                <tr>
-                                                    <td>
-                                                        <a href="/admin/blog/article/edit/{{ \App\Helpers\Helpers::encode($article->id) }}">
-                                                            {{ $article->title }}
-                                                        </a>
-                                                    </td>
-                                                    <td>
-                                                        {{ $article->user->fullname }}
-                                                    </td>
-                                                    <td>
-                                                        {{ $article->blog_category->name }}
-                                                    </td>
-                                                    <td>
-                                                        {{ $article->created_at->diffForHumans() }}
-                                                    </td>
-                                                    <td>
-                                                        <a href="/admin/blog/article/edit/{{ \App\Helpers\Helpers::encode($article->id) }}" class="btn btn-primary rounded btn-sm">
-                                                            <i class="ti ti-pencil"></i>
-                                                        </a>
-                                                        <button class="btn btn-danger rounded btn-sm"
-                                                            onclick="deleteArticle('{{ \App\Helpers\Helpers::encode($article->id) }}')">
-                                                            <i class="ti ti-trash"></i>
-                                                        </button>
-                                                    </td>
-
-                                            @endforeach
-                                            
-                                        </tbody>
-                                    </table>
-                                </div>
+                                @include('admin.blog.tables.list')
                             @else
-                                <div class="text-center">
-                                    <h5>{{__('No posts found')}}</h5>
-                                </div>
+                                @include('app.partials.empty')
                             @endif
                         </div>
                     </div>
                 </div>
-
             </div>
 
         </div>
     </div>
-
 @endsection
