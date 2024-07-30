fc26037335f2151fb993bbe0dee04fa71415f0df
ibnsultan <abdulbasitsultan4@gmail.com>
2024-07-30 10:55:58 +0300

    Mod: minor layout fix

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card">
-                        <div class="card-header position-relative">
+                        <div class="card-header position-relative border-0">
                             <h5>
                                 {{__('Dashboard')}}
                             </h5>                
