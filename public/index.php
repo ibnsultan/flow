89255440f0d3a6ba1d851e2c51aa51f17b6679c2
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-08-17 01:49:12 +0300

    Add: enforce csrf check

--- public/index.php
+++ public/index.php
@@ -23,6 +23,7 @@ chdir(dirname(__DIR__));
 |
 */
 require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/lib/CSRF.php';
 
 /*
 |--------------------------------------------------------------------------
