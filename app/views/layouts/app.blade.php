f2d7af7c76cdcc007ad5f499c1b465e1d04db88f
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-08-17 11:43:16 +0300

    Mod: load simplebar from static assets

--- app/views/layouts/app.blade.php
+++ app/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<title>{{ $settings->site_name }}</title>
-		<!-- [Meta] -->
+		
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -50,7 +50,7 @@
 		<!-- Required Js -->
 		
 		<script src="/assets/js/plugins/jquery-3.7.1.min.js"></script>
-		<script src="https://unpkg.com/simplebar@latest/dist/simplebar.min.js"></script>
+		<script src="/assets/js/simplebar.min.js"></script>
 		<script src="/assets/js/plugins/popper.min.js"></script>
 		<script src="/assets/js/plugins/simplebar.min.js"></script>
 		<script src="/assets/js/plugins/bootstrap.min.js"></script>
