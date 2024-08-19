caed3e530e9359b0ecca774609f9251da13d7403
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-08-20 02:32:40 +0300

    Del: loaded csrf configs from configs

--- public/index.php
+++ public/index.php
@@ -23,7 +23,6 @@ chdir(dirname(__DIR__));
 |
 */
 require dirname(__DIR__) . '/vendor/autoload.php';
-require dirname(__DIR__) . '/lib/CSRF.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -81,6 +80,7 @@ Leaf\View::attach(\Leaf\Blade::class);
 |
 */
 Leaf\Core::loadApplicationConfig();
+
 
 /*
 |--------------------------------------------------------------------------
