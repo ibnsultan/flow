fdcb45f2984f02227486b50ad1d1affebe2ac171
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-08-31 18:09:13 +0300

    add: ability to ignore select seeders

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace App\Database\Seeds;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Lang;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,8 +16,14 @@ class DatabaseSeeder extends Seeder
         // get all classes in the seeder directory
         $classes = array_diff(scandir(__DIR__), ['.', '..', basename(__FILE__)]);
 
-        return array_map(function ($class) {
+        $seederClasses = array_map(function ($class) {
             return __NAMESPACE__ . '\\' . pathinfo($class, PATHINFO_FILENAME);
         }, $classes);
+
+        $ignoredSeeders = [
+            # ExampleSeed::class,
+        ];
+
+        return array_diff($seederClasses, $ignoredSeeders);
     }
 }
